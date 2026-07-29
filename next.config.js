18e3b0489857837ec65ed7503e252b9c9bbd65cc
bridg3alt <brigitmanjooran@gmail.com>
2026-07-29 21:12:18 +0530

    Scaffold Next.js app with Tailwind and TypeScript

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
